8efa71ed5387c5904c81f42b8455b7481c73975a
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-09-12 15:34:36 +0200

    Update torsdag.java

--- Hemläxor/v.37/torsdag.java
+++ Hemläxor/v.37/torsdag.java
@@ -17,6 +17,29 @@ public class torsdag {
 	String str2 = input1.nextLine();
 	System.out.println(str2);
 	
+	Scanner input2 = new Scanner (System.in);
+	System.out.println("Vad är din adress?");
+	
+	String str3 = input2.nextLine();
+	System.out.println(str3);
+	
+	Scanner input3 = new Scanner (System.in);
+	System.out.println("Vad är ditt postnummer?");
+	
+	String str4 = input.nextLine();
+	System.out.println(str4);
+	
+	Scanner input4 = new Scanner (System.in);
+	System.out.println("Vilken stad bor du i?");
+	
+	String str5 = input.nextLine();
+	System.out.println(str5);
+	
+	Scanner input5 = new Scanner (System.in);
+	System.out.println("Vad är ditt tlefonnummer?");
+	
+	String str6 = input.nextLine();
+	System.out.println(str6);
 	}
 	
 	
