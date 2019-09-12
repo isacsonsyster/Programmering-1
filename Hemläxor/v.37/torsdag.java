20ad197de13ac2121b0799e9d8caa1a4aba346db
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-09-12 15:21:37 +0200

    Update torsdag.java

--- Hemläxor/v.37/torsdag.java
+++ Hemläxor/v.37/torsdag.java
@@ -1,7 +1,19 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class torsdag {
-	public state void main(String[] args) {
-}
-	Scanner input = new Scanner (system.in);
 	
+
+	public static void main(String[] args) {
+
+	Scanner input = new Scanner (System.in);
+	System.out.println("Vad heter du?");
+	
+	String str = input.nextLine();
+	System.out.println(str);
+	
+	
+	
+	}
+	
+	
+}
