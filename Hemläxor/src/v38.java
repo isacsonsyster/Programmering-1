d513cff7f9d597d92cdacc8a41514741b638b7ed
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-09-19 10:51:12 +0200

    skriva ut siffror, skriva ut det minsta och det största

---
+++ Hemläxor/src/v38.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class v38 {
+	public static void main(String[] args) {
+		
+		
+		
+		Scanner input1 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra1 = input1.nextInt();
+		
+		Scanner input2 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra2 = input2.nextInt();
+		
+		Scanner input3 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra3 = input3.nextInt();
+
+		Scanner input4 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra4 = input4.nextInt();
+		
+		Scanner input5 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra5 = input5.nextInt();
+		
+		Scanner input6 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra6 = input6.nextInt();
+		
+		Scanner input7 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra7 = input7.nextInt();
+		
+		Scanner input8 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra8 = input8.nextInt();
+		
+		Scanner input9 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra9 = input9.nextInt();
+		
+		Scanner input10 = new Scanner(System.in);
+		System.out.println("Säg en siffra");
+		int siffra10 = input10.nextInt();
+		
+	
+		
+	}
+}
