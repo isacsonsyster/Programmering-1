a9e7afbd0e48e4d34c6fbf51be15b79a17bc010f
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-09-19 10:56:44 +0200

    medelvärdet

--- Hemläxor/src/v38.java
+++ Hemläxor/src/v38.java
@@ -3,49 +3,119 @@ import java.util.Scanner;
 public class v38 {
 	public static void main(String[] args) {
 		
-		
-		
 		Scanner input1 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra1 = input1.nextInt();
+		
+		int max = siffra1;
+		int min = siffra1;
 		
 		Scanner input2 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra2 = input2.nextInt();
 		
+		if (max < siffra2) {
+			max = siffra2;
+		}
+		if (min > siffra2) {
+			min = siffra2;
+		}
+		
 		Scanner input3 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra3 = input3.nextInt();
-
+		
+		if (max < siffra3) {
+			max = siffra3;
+		}
+		if (min > siffra3) {
+			min = siffra3;
+		}
+		
 		Scanner input4 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra4 = input4.nextInt();
+		
+		if (max < siffra4) {
+			max = siffra4;
+		}
+		if (min > siffra4) {
+			min = siffra4;
+		}
+		
 		
 		Scanner input5 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra5 = input5.nextInt();
 		
+		if (max < siffra5) {
+			max = siffra5;
+		}
+		if (min > siffra5) {
+			min = siffra5;
+		}
+		
+		
 		Scanner input6 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra6 = input6.nextInt();
+		
+		if (max < siffra6) {
+			max = siffra6;
+		}
+		if (min > siffra6) {
+			min = siffra6;
+		}
+		
 		
 		Scanner input7 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra7 = input7.nextInt();
 		
+		if (max < siffra7) {
+			max = siffra7;
+		}
+		if (min > siffra7) {
+			min = siffra7;
+		}
+		
+		
 		Scanner input8 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra8 = input8.nextInt();
+		
+		if (max < siffra8) {
+			max = siffra8;
+		}
+		if (min > siffra8) {
+			min = siffra8;
+		}
+		
 		
 		Scanner input9 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra9 = input9.nextInt();
 		
+		if (max < siffra9) {
+			max = siffra9;
+		}
+		if (min > siffra9) {
+			min = siffra9;
+		}
+		
+		
 		Scanner input10 = new Scanner(System.in);
 		System.out.println("Säg en siffra");
 		int siffra10 = input10.nextInt();
 		
-	
+		if (max < siffra10) {
+			max = siffra10;
+		}
+		if (min > siffra10) {
+			min = siffra10;
+		}
+		
+System.out.println((siffra1+siffra2+siffra3+siffra4+siffra5+siffra6+siffra7+siffra8+siffra9+siffra10)/10);
 		
 	}
 }
