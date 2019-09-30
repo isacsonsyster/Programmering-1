400b477ec91a6e95975e3f2a546601a442fdf4e6
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-09-30 11:10:55 +0200

    timeloop

---
+++ Hemläxor/kattisv39/timeloop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+	
+public class timeloop {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);	
+		int n = input.nextInt();
+				
+		for (int i =1; i<= n; i++) {
+			System.out.println(i+ " Abracadabra");
+		}
+	}
+}
+ 
