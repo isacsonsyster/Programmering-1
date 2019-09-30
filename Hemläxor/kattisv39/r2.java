1206437415bc05248f3c9b2950d297226d635fae
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-09-30 11:01:31 +0200

    r2

---
+++ Hemläxor/kattisv39/r2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class r2 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int r1 = input.nextInt();
+		int s = input.nextInt();
+		
+		
+		int r2 = 2*s - r1; 
+		
+		System.out.println(r2);
+				
+		
+		
+	}
+}
