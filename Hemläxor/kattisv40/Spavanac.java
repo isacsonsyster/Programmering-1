f6d7a68cbf783eeb4543bc24c1d5c1c6e17246e8
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-10-03 10:39:10 +0200

    spavanac

--- Hemläxor/kattisv40/Spavanac.java
+++ Hemläxor/kattisv40/Spavanac.java
@@ -9,6 +9,21 @@ public class Spavanac {
 		int H = input.nextInt();
 		
 		int M = input.nextInt();
+			
+		int HM = H * 60;
 		
+		int SummaM = HM + M - 45;
+		
+		int RestM = SummaM % 60;
+			if(RestM < 0)	{
+				RestM = 60 + (RestM);
+				}
+		int RestH = (SummaM - RestM) /60 ;
+			if (RestH < 0) {
+				RestH = 24 + (RestH);
+				}
+			if (H >= 0 && H <= 23 && M >= 0 && M <= 59) {
+				System.out.println(RestH + " " + RestM);
+			}
 		}
 }
