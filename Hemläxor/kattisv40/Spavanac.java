b51ee6e2782375f30952b3656430659855ce55c1
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-10-03 10:22:25 +0200

    Create Spavanac.java

---
+++ Hemläxor/kattisv40/Spavanac.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Spavanac {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		int H = input.nextInt();
+		
+		int M = input.nextInt();
+		
+		}
+}
