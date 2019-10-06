f3ccb0504e0984288c074004ddd48b3d54e76ce9
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-10-06 23:19:21 +0200

    fizzbuzz

---
+++ Hemläxor/kattisv40/FizzBuzz.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+public static void main(String[] args) {
+	
+	Scanner input = new Scanner (System.in);
+	
+	int X = input.nextInt();
+	
+	int Y = input.nextInt();
+	
+	int N = input.nextInt();
+	
+	for(i = 0 )
+}
