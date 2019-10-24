c4a4a20052ba39fae257f8cc30d32cfaa7cb2efc
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-10-24 10:29:30 +0200

    Update metoder.java

--- lektionsuppgifter/v.43/lektionsuppgifter/metoder.java
+++ lektionsuppgifter/v.43/lektionsuppgifter/metoder.java
@@ -2,10 +2,25 @@ package lektionsuppgifter;
 
 public class metoder {
 public static void main (String [] args) {
-	hej("HAHHAHAHAHAH");
+	lol("HAHHAHAHAHAH");
+	System.out.println(min(2,6));
 }
 
-public static void hej(String hej) {
+public static void lol(String hej) {
  System.out.println(hej);
+ 
+public static int min(int a, int b);{
+	if (a <= b) {
+		return a;
+	}
+	else if (b <= a) {
+			return b;
+		}
+	else {
+		return a;
+	}
+}
+
+	 
 }
 }
