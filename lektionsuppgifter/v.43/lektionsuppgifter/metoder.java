616c9c0f9d8bdf1b7ad4bda88702ba8b8b40fbcd
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-10-24 10:18:53 +0200

    metoder 1
    glömde comitta på den här, ska komma ihåg det nästa gång:)
    
    Hade lite problem med denna men jag löste det tillslut

---
+++ lektionsuppgifter/v.43/lektionsuppgifter/metoder.java
@@ -0,0 +1,11 @@
+package lektionsuppgifter;
+
+public class metoder {
+public static void main (String [] args) {
+	hej("HAHHAHAHAHAH");
+}
+
+public static void hej(String hej) {
+ System.out.println(hej);
+}
+}
