2c24b67a02459ed48dd57903ebb36e8feba1877a
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-11-11 10:37:10 +0100

    första metoden+ kommentarer

---
+++ Hemläxor/inlämningsuppgift1/Metoder.java
@@ -0,0 +1,15 @@
+
+public class Metoder {
+	public static void main(String [] args) {
+		
+		System.out.println(fahrenheitToCelcius(50)); 
+	}
+	public static double fahrenheitToCelcius(double fahrenheit) {
+		double fahrenheitToCelcius = (fahrenheit-32) * 5/(9);
+		return fahrenheitToCelcius;
+	}
+	
+		
+}
+
+
