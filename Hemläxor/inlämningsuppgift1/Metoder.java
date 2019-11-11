ccfba23ac1cb4f70c2b6901f58f809b5d7556c69
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-11-11 10:43:15 +0100

    metod 2 + kommentarer

--- Hemläxor/inlämningsuppgift1/Metoder.java
+++ Hemläxor/inlämningsuppgift1/Metoder.java
@@ -2,11 +2,21 @@
 public class Metoder {
 	public static void main(String [] args) {
 		
-		System.out.println(fahrenheitToCelcius(50)); 
+		System.out.println(kelvinToCelcius(0));
+		
 	}
+	/**
+	 * metoden räknar ut celcius från fahrenheit
+	 * @param fahrenheit värdet i fahrenheit
+	 * @return värdet i celcius
+	 */
 	public static double fahrenheitToCelcius(double fahrenheit) {
-		double fahrenheitToCelcius = (fahrenheit-32) * 5/(9);
+		double fahrenheitToCelcius = (fahrenheit-32) * 5/(9); 
 		return fahrenheitToCelcius;
+	}
+	public static double kelvinToCelcius(double kelvin) {
+		double kelvinToCelcius = kelvin - 273.15;
+		return kelvinToCelcius;
 	}
 	
 		
