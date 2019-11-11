2ecf4eb36b3158b902b52309b0ae08077717bc78
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2019-11-11 11:14:48 +0100

    3 metoden + kommentarer

--- Hemläxor/inlämningsuppgift1/Metoder.java
+++ Hemläxor/inlämningsuppgift1/Metoder.java
@@ -1,9 +1,11 @@
+import enums.FluidTable;
 
 public class Metoder {
 	public static void main(String [] args) {
 		
-		System.out.println(kelvinToCelcius(0));
 		
+	System.out.println(fluidPressure(FluidTable.WATER, 10));
+	
 	}
 	/**
 	 * metoden räknar ut celcius från fahrenheit
@@ -14,11 +16,19 @@ public class Metoder {
 		double fahrenheitToCelcius = (fahrenheit-32) * 5/(9); 
 		return fahrenheitToCelcius;
 	}
+	/**	
+	 * metoden räknar ut celcius från Kelvin 
+	 * @param kelvin värdet i kelvin
+	 * @return värdet i celcius 
+	 */
 	public static double kelvinToCelcius(double kelvin) {
 		double kelvinToCelcius = kelvin - 273.15;
 		return kelvinToCelcius;
 	}
-	
+	public static double fluidPressure(FluidTable fluid, double deep) {
+		double fluidPressure = fluid.density*9.82*deep;
+		return fluidPressure;
+	}
 		
 }
 
--- Hemläxor/inlämningsuppgift1/enums/FluidTable.java
+++ Hemläxor/inlämningsuppgift1/enums/FluidTable.java
@@ -6,14 +6,14 @@ public enum FluidTable {
 	WATER(0.998, 4.19, 0, 100, 2260),
 	H2SO4(1.84, 1.38, 10, 336, 511),
 	KVICKSILVER(13.546, 0.14, -39, 357, 296),
-	ACETON(0.790, 2.20, -95, 56, 509),
+	ACETON(0.790, 2.20, -95, 56, 509),	
 	;
 
-	double density;
-	double heatCapacity;
-	double meltPoint;
-	double boilPoint;
-	double steamEntalpy;
+	public double density;
+	public double heatCapacity;
+	public double meltPoint;
+	public double boilPoint;
+	public double steamEntalpy;
 	
 	FluidTable(double d, double hc, double sp, double bp, double se) {
 		density = d * 1E3;
