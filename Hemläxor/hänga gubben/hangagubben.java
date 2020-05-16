1f27d3bd922986f34ca44708f72b0edccf4c5234
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2020-05-16 16:04:57 +0200

    metoder

--- Hemläxor/hänga gubben/hangagubben.java
+++ Hemläxor/hänga gubben/hangagubben.java
@@ -7,8 +7,8 @@ public class hangagubben {
 	public static Scanner input = new Scanner (System.in);
 	public static Random random = new Random();
 	public static char[] wordToGuess; 
-	public static int guessesLeft;
 	public static char[] guessedWord;
+	public static int guessesLeft;
 	public static int correctWord;
 	public static ArrayList<Character> guessedLetters = new ArrayList<Character>();
 	public static char guessedLetter;
@@ -40,24 +40,27 @@ public class hangagubben {
 		
 		if (chooseDifficulty == 1){
 			easyLevel();
+			guessesLeft = 15;
 		}
 		else if (chooseDifficulty == 2) {
 			mediumLevel();
+			guessesLeft = 11;
 		}
 
 		else if (chooseDifficulty == 3){
 			hardLevel();
+			guessesLeft = 8;
 		}
 	}
 	public static void playGame() {
-		guessedWord ();
+		guessedWordSoFar ();
 		System.out.println("ordet du ska gissa på är " + wordToGuess.length + "bokstäver långt");
 		
 		boolean guessedLetter = false;
 		while (!guessedLetter && guessesLeft!=0) {
 			checkIfRight();
 			guessedLetter = doneWithWord();
-			System.out.println("Ordet" + Arrays.toString(guessedWord));
+			System.out.println("Ordet" + Arrays.toString(wordToGuess));
 			if (guessedLetters.size() == 0) {
 				System.out.println("Du har alla dina gissningar kvar." + guessesLeft);
 				
@@ -66,6 +69,11 @@ public class hangagubben {
 					System.out.println("du har" + guessesLeft + "gissningar kvar");
 				}
 			}
+		if (guessesLeft == 0) {
+			System.out.println("Tyvärr du förlorade, dina gissningar tog slut");
+			System.out.println("det rätta ordet var");
+			System.out.println(wordToGuess);
+		}
 		}
 		
 	
@@ -74,10 +82,11 @@ public class hangagubben {
 	
 	/**
 	 * this method will check if the letter the player are guessing are in the word or not
+	 * @return 
 	 * @return if the letter was right or not 
 	 */
 	
-	public static void checkIfRight(){
+	public static int checkIfRight(){
 		boolean rightLetter = false;
 		for(int i = 0; i < wordToGuess.length; i++) {
 			if (wordToGuess[i] == guessedLetter) {
@@ -85,34 +94,36 @@ public class hangagubben {
 				guessedWord[i] = guessedLetter;
 				rightLetter = true;
 				
-				if(rightLetter = false) {
+				if( rightLetter = false) {
 					System.out.println("Tyvärr, du gissade fel, testa att gissa en annan bokstav");
 					if(guessedLetters.contains(guessedLetter)) {
 					}
 					else {
-						guessedLetters.add(guessedLetter){
+						guessedLetters.add(guessedLetter);
+						guessesLeft -- ; 
 						}
 					}
 				}
 			}
+		return guessesLeft;
+		
 		}
-	}
-
+	
 		
 	
 	
-	public static void easyLevel(){
+	public static char[] easyLevel(){
 		String [] easyWords = {
 		"Trilla","Sill","Klass","Kallare","Hårborste","Naglar","Kommer","Mycket","Sverige","Stora","Inget","Samma",
 		"Människor","Europa","Barnen","Händer","Kallelse"};
 		System.out.println("Du valde den lätta nivån.");
 		System.out.println("Du kommer ha 15 gissningar på dig att gissa rätt ord");
 		System.out.println("Kör hårt och lycka till!");
-		int guessesLeft = 15;
 		char[] wordToGuess = easyWords[random.nextInt(easyWords.length)].toCharArray();
+		return wordToGuess;
 	}
 	
-	public static void mediumLevel(){
+	public static char[] mediumLevel(){
 		String[] mediumWords = {"Företaget", "Skola", "Dator", "Hårsnodd", "Såga", "Lat", "Bläckpenna",
 		"Förlåt", "Men", "Virknål", "Körskola", "Kyrka", "Maten", 
 		"Måttband", "Galler", "Grav", "Inlagd"};
@@ -120,11 +131,11 @@ public class hangagubben {
 		System.out.println("Du kommer ha 11 gissningar på dig att gissa rätt ord");
 		System.out.println("Det är svårare ord än den lätta nivån");
 		System.out.println("Lycka till");
-		int guessesLeft = 11;
 		char[] wordToGuess = mediumWords[random.nextInt(mediumWords.length)].toCharArray();
+		return wordToGuess;
 	}
 	
-	public static void hardLevel (){
+	public static char[] hardLevel (){
 		String [] hardWords = { "Jazz", "Flaggstång", "Extra", "Vispgrädde", "Portion", "Xylofon", 
 		"Psykolog", "Krokodiltårar", "Croissanter", "Squash", "Nationalencyklopedin", 
 		"Justitieminister", "Växsjö", "Hundkoja", "Yatzy",  "Ukulele", "Yxa", "Oxe", "Taxi",};
@@ -133,13 +144,12 @@ public class hangagubben {
 		System.out.println("Använd dem väl");
 		System.out.println("Det är väldigt mycket svårare ord än medel och lätta nivån");
 		System.out.println("Lycka till, du kommer behöva det");
-		int guessesLeft = 8;
 		char [] wordToGuess = hardWords[random.nextInt(hardWords.length)].toCharArray(); // slumpar ett ord från en arraylist och lägger in i en chararray så att man kan gissa på ordet.// 
-
+		return wordToGuess;
 	}
 	
-	public static void guessedWord() {
-		guessedWord = new char [wordToGuess.length];
+	public static void guessedWordSoFar() {
+		guessedWord = new char [wordToGuess.length()];
 		for (int i = 0; i < guessedWord.length; i++) {
 			guessedWord [i] = '_';
 		}
@@ -162,7 +172,7 @@ public class hangagubben {
 		System.out.println("tryck 3 för att byta svårighetsgrad");
 		
 		if (chooseAgain == 1) {
-			break;
+			
 		}
 			else if (chooseAgain == 2) {
 				playGame();
