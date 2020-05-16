588358eead096fce13bc184e59c00f09aa0b9b30
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2020-05-16 11:15:43 +0200

    gjort arraylists och fler metoder

--- Hemläxor/hänga gubben/hangagubben.java
+++ Hemläxor/hänga gubben/hangagubben.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class hangagubben {
 	public static void main(String[] args) {
@@ -7,8 +9,84 @@ public class hangagubben {
 		System.out.println("Hej och välkommen till hänga gubben.");
 	
 	
-	
-	
+		ArrayList<String> easyWords = new ArrayList<String>();
+		easyWords.add("Trilla");
+		easyWords.add("Sill");
+		easyWords.add("Klass");
+		easyWords.add("Kallare");
+		easyWords.add("Hårborste");
+		easyWords.add("Naglar");
+		easyWords.add("Kommer");
+		easyWords.add("Mycket");
+		easyWords.add("Sverige");
+		easyWords.add("Stora");
+		easyWords.add("Inget");
+		easyWords.add("Samma");
+		easyWords.add("Människor");
+		easyWords.add("Europa");
+		easyWords.add("Barnen");
+		easyWords.add("Händer");
+		easyWords.add("Kallelse");
+
+		
+		ArrayList<String> mediumWords = new ArrayList<String>();
+		mediumWords.add("Företaget");
+		mediumWords.add("Skola");
+		mediumWords.add("Dator");
+		mediumWords.add("Hårsnodd");
+		mediumWords.add("Såga");
+		mediumWords.add("Lat");
+		mediumWords.add("Inlagd");
+		mediumWords.add("Säga");
+		mediumWords.add("Låt");
+		mediumWords.add("Förlåt");
+		mediumWords.add("Men");
+		mediumWords.add("Anteckningsblock");
+		mediumWords.add("Måttband");
+		mediumWords.add("Dalarna");
+		mediumWords.add("Virknål");
+		mediumWords.add("Sticka");
+		mediumWords.add("Galler");
+		mediumWords.add("Bläckpenna");
+		mediumWords.add("Tråd");
+		mediumWords.add("Äta");
+		mediumWords.add("Körskola");
+		mediumWords.add("Kyrka");
+		mediumWords.add("Grav");
+		mediumWords.add("Maten");
+		
+		ArrayList<String> hardWords = new ArrayList<String>();
+		hardWords.add("Jazz");
+		hardWords.add("Flaggstång");
+		hardWords.add("Extra");
+		hardWords.add("Vispgrädde");
+		hardWords.add("Ukulele");
+		hardWords.add("Saxofon");
+		hardWords.add("Xylofon");
+		hardWords.add("Portion");
+		hardWords.add("Yatzy");
+		hardWords.add("Psykolog");
+		hardWords.add("Marabou");
+		hardWords.add("Hundkoja");
+		hardWords.add("Yxa");
+		hardWords.add("Oxe");
+		hardWords.add("Vas");
+		hardWords.add("Krokodiltårar");
+		hardWords.add("Häst");
+		hardWords.add("Croissanter");
+		hardWords.add("Vibrationer");
+		hardWords.add("Byxa");
+		hardWords.add("Taxi");
+		hardWords.add("Squash");
+		hardWords.add("Nationalencyklopedin");
+		hardWords.add("Epidemiologi");
+		hardWords.add("Justitieminister");
+		hardWords.add("Växsjö");
+
+
+
+
+
 	
 	
 	}
@@ -22,12 +100,17 @@ public class hangagubben {
 		for(i=0; i<wordToGuess; i++) {
 			if (wordToGuess[i] = guessedLetter) {
 				System.out.println("Bra, du gissade rätt!");
-				
+				correctWord[i] = guessedLetter;
 				rightLetter = true;
 				
 				if(rightLetter = false) {
 					System.out.println("Tyvärr, du gissade fel, testa att gissa en annan bokstav");
-					if()
+					if(wrongLetters.contains(guessedLetter)) {
+					}
+					else {
+						wrongLetters.add(guessedLetter){
+						}
+					}
 				}
 			}
 		}
@@ -37,6 +120,11 @@ public class hangagubben {
 	}
 	
 	public static void easyLevel{
+		System.out.println("Du valde den lätta nivån.");
+		System.out.println("Du kommer ha 15 gissningar på dig att gissa rätt ord");
+		System.out.println("Kör hårt och lycka till!");
+		
+		wordToGuess = easyWords[random.nextInt(easyWords.length).toCharArray]
 	}
 	
 	public static void mediumLevel{
