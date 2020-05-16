755996a94ea5d0e7c45cd0566b5284c0a3ccdfcb
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2020-05-16 11:15:23 +0200

    började koda första metoden

---
+++ Hemläxor/hänga gubben/hangagubben.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class hangagubben {
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner (System.in);
+		System.out.println("Hej och välkommen till hänga gubben.");
+	
+	
+	
+	
+	
+	
+	}
+	/**
+	 * this method will check if the letter the player are guessing are in the word or not
+	 * @return if the letter was right or not 
+	 */
+	
+	public static boolean checkIfRight(){
+		boolean rightLetter = false;
+		for(i=0; i<wordToGuess; i++) {
+			if (wordToGuess[i] = guessedLetter) {
+				System.out.println("Bra, du gissade rätt!");
+				
+				rightLetter = true;
+				
+				if(rightLetter = false) {
+					System.out.println("Tyvärr, du gissade fel, testa att gissa en annan bokstav");
+					if()
+				}
+			}
+		}
+	}
+	public static Array guessedWordSoFar{
+		
+	}
+	
+	public static void easyLevel{
+	}
+	
+	public static void mediumLevel{
+		
+	}
+	
+	public static void hardLevel
+}
+
